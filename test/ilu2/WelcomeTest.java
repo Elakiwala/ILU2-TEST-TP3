fa0035bd1308d942aaad633d175f129cf739fa41
Elakiwala <evamartha.lansalot@gmail.com>
2023-04-21 09:42:06 +0200

    EX_1

---
+++ test/ilu2/WelcomeTest.java
@@ -0,0 +1,58 @@
+package ilu2;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class WelcomeTest {
+
+	@Test
+	void EX_1() {
+		assertEquals(Welcome.welcome("bob"), "Hello, Bob");
+	}
+	
+	@Test
+	void EX_2() {
+		//TODO
+	}
+
+	@Test
+	void EX_3() {
+		//TODO
+	}
+	
+	@Test
+	void EX_4() {
+		//TODO
+	}
+	
+	@Test
+	void EX_5() {
+		//TODO
+	}
+	
+	@Test
+	void EX_6() {
+		//TODO
+	}
+	
+	@Test
+	void EX_7() {
+		//TODO
+	}
+	
+	@Test
+	void EX_8() {
+		//TODO
+	}
+	
+	@Test
+	void EX_9() {
+		//TODO
+	}
+	
+	@Test
+	void EX_10() {
+		//TODO
+	}
+}
